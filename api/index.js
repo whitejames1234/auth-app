cc3c3ea581708cd16ec8b6fd7367d7fbdbb24653
Rida Labiad <115871096+whitejames1234@users.noreply.github.com>
2024-07-29 22:36:03 +0200

    connect db

--- api/index.js
+++ api/index.js
@@ -1,12 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+
+import 'dotenv/config'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT ;
+const dbUrl = process.env.DB_URL ;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 👻 😂😂')
+  res.send('Hello World! 👻 😂😂 🦧')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+
+mongoose.connect(dbUrl)
+  .then(() => {
+    console.log('Bd is conected');
+    app.listen(port || 4000 , () => {
+      console.log(`Example app listening on port ${port}`)
+    })
+   }).catch((err)=>{
+    console.log(err)
+  });
