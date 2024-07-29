b24284c2e69fa60882eacf8bb09a1510cc5bb0e3
Rida Labiad <115871096+whitejames1234@users.noreply.github.com>
2024-07-29 22:41:27 +0200

    update

--- api/index.js
+++ api/index.js
@@ -2,16 +2,17 @@ import express from "express";
 import mongoose from "mongoose";
 
 import 'dotenv/config'
-
 const app = express()
-const port = process.env.PORT ;
-const dbUrl = process.env.DB_URL ;
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 👻 😂😂 🦧')
+  res.send('Hello World! 👻')
 })
 
 
+//======== conect DB ========//
+const port = process.env.PORT ;
+const dbUrl = process.env.DB_URL ;
 
 mongoose.connect(dbUrl)
   .then(() => {
